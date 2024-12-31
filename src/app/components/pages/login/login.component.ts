83b1ccc6ffc376352b71b6c0a881e456f4efd63d
Jyoti <jyoti.sdet11@gmail.com>
2024-12-31 12:08:12 +0530

    Added Login component & Environment files

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/dto/user.model';
+import { LoaderService } from 'src/app/service/common/loader.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  user = new User('', '', '', '', '', '', '');
+
+  constructor(private _tostr: ToastrService, private _userService: UserService, public _loader: LoaderService) { }
+
+  onLoginSubmit(event: SubmitEvent, signupForm: NgForm) {}
 
 }
